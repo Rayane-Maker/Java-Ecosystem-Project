39ecf5086228bbd9452ad93c450b6e598e78faac
Azo1hz <antoine.diezlatteur@gmail.com>
2024-01-06 13:44:53 +0100

    Antoine

--- Plant.java
+++ Plant.java
@@ -1,2 +1,14 @@
-public class Plant {
+public class Plant extends GameObject {
+    private Boolean eatable;
+    private Double nutriscore;
+
+    public Plant(Double nutriscore, Boolean eatable) {
+        this.eatable = eatable;
+        this.nutriscore = nutriscore;
+    }
+
+    public void displayInfo() {
+        System.out.println("I am eatable :" + this.eatable + " (nutriscore :" + this.nutriscore + ")");
+    }
 }
+
